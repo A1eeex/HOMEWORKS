8bf66f1467f067e71bb32d33c7cacef304d40e0a
Alexandr <75209103+A1eeex@users.noreply.github.com>
2021-01-28 23:08:02 +0200

    Hm-1

---
+++ HM-1/js/costom.js
@@ -0,0 +1,12 @@
+const Question = prompt ('What is Your Name?');
+const respondQuestionFirst = ('Hello, ');
+const respondQuestionSecond = ("! How are you?")
+
+alert (respondQuestionFirst + Question+ respondQuestionSecond);
+
+
+
+// const name = prompt('What is your name');
+// alert('Helo, ' + name + '! How are you');
+
+
